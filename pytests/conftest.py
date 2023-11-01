bd10ae850af68bffa0da97537fb3520307d9b375
rus play <nikolaenkoruslan011@gmail.com>
2023-11-01 20:48:34 +0300

    added matrix strg with linux & Edge

--- pytests/conftest.py
+++ pytests/conftest.py
@@ -13,15 +13,15 @@ def browser():
     """
     basic fixture
     """
-    # chrome_options = Options()
+    chrome_options = Options()
     # chrome_options.add_argument("--no-sandbox")
     # chrome_options.add_argument("start-maximized")
     # chrome_options.add_argument("--disable-infobars")
     # chrome_options.add_argument("--disable-extensions")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
 
     # s = Service(ChromeDriverManager().install())
-    driver = webdriver.ChromiumEdge()
+    driver = webdriver.ChromiumEdge(options=chrome_options)
 
     # it uses for hard shot down a browser by unforeseen mistakes
     yield driver

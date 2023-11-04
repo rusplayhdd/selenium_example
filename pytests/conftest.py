27e588eede2724dae7fbae30914a9492b325d7f5
rus play <nikolaenkoruslan011@gmail.com>
2023-11-04 16:59:40 +0300

    bgfds

--- pytests/conftest.py
+++ pytests/conftest.py
@@ -4,7 +4,7 @@ fixture
 import pytest
 import selenium.webdriver.firefox.options
 from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -14,15 +14,15 @@ def browser():
     """
     basic fixture
     """
-    chrome_options = webdriver.FirefoxOptions()
+    chrome_options = webdriver.EdgeOptions()
     chrome_options.add_argument("--no-sandbox")
     # chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
 
     # s = Service(ChromeDriverManager().install())
-    driver = webdriver.Firefox(options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     # it uses for hard shot down a browser by unforeseen mistakes
     yield driver
---
+++ vvv.py
@@ -0,0 +1,5 @@
+import os
+
+print(os.path.abspath("test_img\\OIP.jpg"))
+
+# print(os.getcwd('test_img\OIP.jpg'))
